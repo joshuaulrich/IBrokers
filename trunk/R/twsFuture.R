dfd28d7a285c4730e9c9c67d9d7eac46c92a75a1
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2008-02-24 17:51:13 +0000

    Updates to twsFuture Added combo_legs

--- trunk/R/twsFuture.R
+++ trunk/R/twsFuture.R
@@ -3,6 +3,6 @@ function(symbol,exch,expiry,primary='',
          currency='USD',right='',local='',multiplier='',include_expired='0')
 {
   twsContract(symbol,'FUT',exch,primary,expiry,strike='0.0',
-              currency,right,local,multiplier,include_expired)
+              currency,right,local,multiplier,NULL,NULL,include_expired)
 }
 
