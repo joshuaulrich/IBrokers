86660b3099e83485627b600e6ca217d2886ff608
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-03-13 15:18:57 +0000

    new parser base function new twsConnect2 with parser functionality !--not production--!

--- R/twsConnect.R
+++ R/twsConnect.R
@@ -1,3 +1,33 @@
+`twsConnect2` <-
+function (port = 7496, clientID = 1)
+ {
+     s <- socketConnection(port = port)
+     writeChar("37", s)
+     writeChar(as.character(clientID), s)
+     waiting <- TRUE
+     while (1) {
+         curChars <- readChar(s,1000)
+         #curChar <- readChar(s, 1)
+         if (waiting & identical(curChars, character(0)))
+             next
+         waiting <- FALSE
+ 
+         # process what is captured thus far
+         pc <- parseTWS(curChars)
+         SERVERINFO <- FALSE
+ 
+         if(!SERVERINFO & length(pc) > 0) {
+           SERVER_VERSION <- pc[1]
+           CONNECTION_TIME <- pc[2]
+           SERVERINFO <- TRUE
+         }
+ 
+         if (identical(curChars, character(0)))
+             break
+     }
+     structure(list(s,SERVER_VERSION,CONNECTION_TIME), class = "twsConnection")
+ }
+
 `twsConnect` <-
 function(port=7496,clientID=1) {
   s <- socketConnection(port=port)
