cdc3010e22a000ad350b04b04378429f7773cfb1
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-04-29 20:55:13 +0000

    small errorHandler changes dev of reqMktData new eventHandler for default MktData reply WISHLIST exported [experimental!!!] reqMktData and reqRealTimeBars

---
+++ R/eventHandlers.R
@@ -0,0 +1,24 @@
+`e_tick_price`    <- function(msg,string) {
+  cat('<default tickPrice> ')
+  cat(msg,paste(string),'\n')
+}
+
+`e_tick_size`    <- function(msg,string) {
+
+}
+
+`e_tick_option`  <- function(msg,string) {
+
+}
+
+`e_tick_generic` <- function(con) {
+
+}
+
+`e_tick_string`  <- function(con) {
+
+}
+
+`e_tick_EFP`     <- function(con) {
+
+}
