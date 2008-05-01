6b0d73cca6084e2d68eeaa5096fdcc08e73e1a01
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2008-05-01 21:21:48 +0000

    new version #8 added file arg to reqMktData to utilize underlying *cat* functionality of piping/etc.  Very crude, but usable new timeStamps in data for reqMktData as well

--- trunk/R/eventHandlers.R
+++ trunk/R/eventHandlers.R
@@ -1,129 +1,151 @@
-`e_tick_price`    <- function(msg,string) {
+`e_tick_price`    <- function(msg,string,timeStamp,file,...) {
   tickType <- string[3]
-  cat("id=",string[2]," ",sep='')
+  if(!is.null(timeStamp)) cat('<',format(Sys.time(),timeStamp),'>',sep='',file=file,append=TRUE)
+  cat(" id=",string[2]," ",sep='',file=file,append=TRUE)
   if(tickType == .twsTickType$BID) {
-    cat('bidPrice:',string[4],' ')
-    cat('bidSize:',string[5],'\n')
-  }
+    cat('bidPrice:',string[4],' ',file=file,append=TRUE)
+    cat('bidSize:',string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$ASK) {
-    cat('askPrice:',string[4],' ')
-    cat('askSize:',string[5],'\n')
-  }
+    cat('askPrice:',string[4],' ',file=file,append=TRUE)
+    cat('askSize:',string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LAST) {
-    cat('lastPrice:',string[4],'\n')
-  }
+    cat('lastPrice:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$HIGH) {
-    cat('highPrice:',string[4],'\n')
-  }
+    cat('highPrice:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LOW) {
-    cat('lowPrice:',string[4],'\n')
-  }
+    cat('lowPrice:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$CLOSE) {
-    cat('closePrice:',string[4],'\n')
-  }
+    cat('closePrice:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$OPEN) {
-    cat('openPrice:',string[4],'\n')
-  }
+    cat('openPrice:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LOW_13_WEEK) {
-    cat('13-week Low:',string[4],'\n')
-  }
+    cat('13-week Low:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$HIGH_13_WEEK) {
-    cat('13-week High:',string[4],'\n')
-  }
+    cat('13-week High:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LOW_26_WEEK) {
-    cat('26-week Low:',string[4],'\n')
-  }
+    cat('26-week Low:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$HIGH_26_WEEK) {
-    cat('26-week High:',string[4],'\n')
-  }
+    cat('26-week High:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LOW_52_WEEK) {
-    cat('52-week Low:',string[4],'\n')
-  }
+    cat('52-week Low:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$HIGH_52_WEEK) {
-    cat('52-week High:',string[4],'\n')
+    cat('52-week High:',string[4],'\n',file=file,append=TRUE)
+  } else {
+    # something missed?? 
+    cat('<default tickPrice> ')
+    cat(msg,paste(string),'\n',file=file,append=TRUE)
   }
-  
-  #cat('<default tickPrice> ')
-  #cat(msg,paste(string),'\n')
 }
 
-`e_tick_size`    <- function(msg,string) {
+`e_tick_size`    <- function(msg,string,timeStamp,file,...) {
   tickType <- string[3] 
-  cat("id=",string[2]," ",sep='')
+  if(!is.null(timeStamp)) cat('<',format(Sys.time(),timeStamp),'>',sep='',file=file,append=TRUE)
+  cat(" id=",string[2]," ",sep='',file=file,append=TRUE)
   if(tickType == .twsTickType$BID_SIZE) {
-    cat('bidSize:',string[4],'\n')
-  }
+    cat('bidSize:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$ASK_SIZE) {
-    cat('askSize:',string[4],'\n')
-  }
+    cat('askSize:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LAST_SIZE) {
-    cat('lastSize:',string[4],'\n')
-  }
+    cat('lastSize:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$VOLUME) {
-    cat('Volume:',string[4],'\n')
-  }
+    cat('Volume:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$AVG_VOLUME) {
-    cat('averageVolume:',string[4],'\n')
-  }
+    cat('averageVolume:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$OPTION_CALL_OPEN_INTEREST) {
-    cat('optionCallOpenInterest:',string[4],'\n')
-  }
+    cat('optionCallOpenInterest:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$OPTION_PUT_OPEN_INTEREST) {
-    cat('optionPutOpenInterest:',string[4],'\n')
-  }
+    cat('optionPutOpenInterest:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$OPTION_CALL_VOLUME) {
-    cat('optionCallVolume:',string[4],'\n')
-  }
+    cat('optionCallVolume:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$OPTION_PUT_VOLUME) {
-    cat('optionPutVolume:',string[4],'\n')
+    cat('optionPutVolume:',string[4],'\n',file=file,append=TRUE)
+  } else {
+    cat('<default size> ',file=file,append=TRUE)
+    cat(paste(string),'\n',file=file,append=TRUE)
   }
 }
 
-`e_tick_option`  <- function(msg,string) {
+`e_tick_option`  <- function(msg,string,timeStamp,file,...) {
   tickType <- string[3] 
-  cat("id=",string[2]," ",sep='')
+  if(!is.null(timeStamp)) cat('<',format(Sys.time(),timeStamp),'>',sep='',file=file,append=TRUE)
+  cat(" id=",string[2]," ",sep='',file=file,append=TRUE)
   if(tickType == .twsTickType$BID_OPTION) { #10
-    cat('bidOption:',string[4],string[5],'\n')
-  }
+    cat('bidOption:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$ASK_OPTION) { #11
-    cat('askOption:',string[4],string[5],'\n')
-  }
+    cat('askOption:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LAST_OPTION) { #12
-    cat('lastOption:',string[4],string[5],'\n')
-  }
+    cat('lastOption:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$MODEL_OPTION) { #13
-    cat('modelOption:',string[4],string[5],'\n')
+    cat('modelOption:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else {
+    cat('<default option> ',file=file,append=TRUE)
+    cat(paste(string),'\n',file=file,append=TRUE)
   }
-  if(tickType == .twsTickType$OPTION_HISTORICAL_VOL) { #23
-    cat('optionHistoricalVol:',string[4],string[5],'\n')
-  }
+}
+
+`e_tick_generic` <- function(msg,string,timeStamp,file,...) {
+  tickType <- string[3] 
+  if(!is.null(timeStamp)) cat('<',format(Sys.time(),timeStamp),'>',sep='',file=file,append=TRUE)
+  cat(" id=",string[2]," ",sep='',file=file,append=TRUE)
   if(tickType == .twsTickType$OPTION_IMPLIED_VOL) { #24
-    cat('optionImpliedVol:',string[4],string[5],'\n')
-  }
-}
-
-`e_tick_generic` <- function(msg,string) {
-  tickType <- string[3] 
-  cat("id=",string[2]," ",sep='')
+    cat('indexFuturePremium:',string[4],'\n',file=file,append=TRUE)
+  } else 
+  if(tickType == .twsTickType$OPTION_HISTORICAL_VOL) { #23
+    cat('optionHistoricalVol:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$INDEX_FUTURE_PREMIUM) { #31
-    cat('indexFuturePremium:',string[4],'\n')
+    cat('optionImpliedVol:',string[4],string[5],'\n',file=file,append=TRUE)
+  } else
+  if(tickType == .twsTickType$SHORTABLE) { #46
+    cat('shortable:',string[4],'\n',file=file,append=TRUE)
+  } else {
+    cat('<default generic> ',file=file,append=TRUE)
+    cat(paste(string),'\n',file=file,append=TRUE)
   }
 }
 
-`e_tick_string`  <- function(msg,string) {
+`e_tick_string`  <- function(msg,string,timeStamp,file,...) {
   tickType <- string[3] 
-  cat("id=",string[2]," ",sep='')
+  if(!is.null(timeStamp)) cat('<',format(Sys.time(),timeStamp),'>',sep='',file=file,append=TRUE)
+  cat(" id=",string[2]," ",sep='',file=file,append=TRUE)
   if(tickType == .twsTickType$BID_EXCH) { #32
-    cat('bidExchange:',string[4],'\n')
-  }
+    cat('bidExchange:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$ASK_EXCH) { #33
-    cat('askExchange:',string[4],'\n')
-  }
+    cat('askExchange:',string[4],'\n',file=file,append=TRUE)
+  } else
   if(tickType == .twsTickType$LAST_TIMESTAMP) { #45
-    cat('lastTimestamp:',string[4],'\n')
+    cat('lastTimestamp:',string[4],'\n',file=file,append=TRUE)
+  } else {
+    cat('<default string> ',file=file,append=TRUE)
+    cat(paste(string),'\n',file=file,append=TRUE)
   }
 }
 
-`e_tick_EFP`     <- function(con) {
-
+`e_tick_EFP`     <- function(msg,contents,file,append=TRUE) {
+    cat('<default EFP> ',file=file,append=TRUE)
+    cat(paste(string),'\n',file=file,append=TRUE)
 }
--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -1,4 +1,4 @@
 `.onLoad` <- function(lib,pkg) {
-  cat("IBrokers version 0.0-7: (pre-alpha)\n")
+  cat("IBrokers version 0.0-8: (pre-alpha)\n")
   cat("See ?IBrokers for details\n")
 }
