843b1d644917ea2a7762178ddc363ff1be22bea4
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-05-01 21:21:48 +0000

    new version #8 added file arg to reqMktData to utilize underlying *cat* functionality of piping/etc.  Very crude, but usable new timeStamps in data for reqMktData as well

--- R/reqMktData.R
+++ R/reqMktData.R
@@ -1,8 +1,10 @@
 `reqMktData` <-
 function (conn, Contract, tickGenerics='100,101,104,106,162,165,221,225,236',
-          snapshot = FALSE, tickerId = "1", timeStamp=TRUE, callback, file, verbose=TRUE,
-          eventTickPrice,eventTickSize,eventTickOption,eventTickGeneric,
-          eventTickString,eventTickEFP) 
+          snapshot = FALSE, tickerId = "1", timeStamp="%Y%m%d %H:%M:%OS",
+          file='', verbose=TRUE,
+          eventTickPrice,eventTickSize,
+          eventTickOption,eventTickGeneric,
+          eventTickString,eventTickEFP,CALLBACK,...) 
 {
     if (class(conn) != "twsConnection") 
         stop("tws connection object required")
@@ -26,19 +28,23 @@ function (conn, Contract, tickGenerics='100,101,104,106,162,165,221,225,236',
       }
     }
 
-    # set up default event handlers
-    if(missing(eventTickPrice))
-      eventTickPrice <- e_tick_price
-    if(missing(eventTickSize))
-      eventTickSize  <- e_tick_size
-    if(missing(eventTickOption)) 
-      eventTickOption <- e_tick_option
-    if(missing(eventTickGeneric)) 
-      eventTickGeneric <- e_tick_generic
-    if(missing(eventTickString)) 
-      eventTickString <- e_tick_string
-    if(missing(eventTickEFP))
-      eventTickEFP <- e_tick_EFP
+
+    # set up default event handlers, if
+    # callback is not set
+    if(missing(CALLBACK)) {
+      if(missing(eventTickPrice))
+        eventTickPrice <- e_tick_price
+      if(missing(eventTickSize))
+        eventTickSize  <- e_tick_size
+      if(missing(eventTickOption)) 
+        eventTickOption <- e_tick_option
+      if(missing(eventTickGeneric)) 
+        eventTickGeneric <- e_tick_generic
+      if(missing(eventTickString)) 
+        eventTickString <- e_tick_string
+      if(missing(eventTickEFP))
+        eventTickEFP <- e_tick_EFP
+    }
 
     snapshot <- ifelse(snapshot,"1","0")
 
@@ -67,70 +73,52 @@ function (conn, Contract, tickGenerics='100,101,104,106,162,165,221,225,236',
 
     if (.Platform$OS == "windows") 
         Sys.sleep(0.1)
-    while (waiting) {
+    if(missing(CALLBACK)) {
+      while (waiting) {
         curMsg <- suppressWarnings(readBin(con, character(), 
             1))
         if (length(curMsg) > 0) {
-            if (curMsg == .twsIncomingMSG$ERR_MSG) {
-                if (!errorHandler(con, verbose, OK = c(165, 300, 2104,2106,2107))) {
-                  cat("\n")
-                  stop("Unable to complete market data request")
-                }
-            }
-            if (curMsg == .twsIncomingMSG$TICK_PRICE) {
-                contents <- readBin(con, character(), 6)
-                eventTickPrice(curMsg,contents)
-            }
-            if (curMsg == .twsIncomingMSG$TICK_SIZE) {
-                contents <- readBin(con, character(), 4)
-                eventTickSize(curMsg,contents)
-            }
-            if (curMsg == .twsIncomingMSG$TICK_OPTION) {
-                contents <- readBin(con, character(), 5)
-                eventTickOption(curMsg,contents)
-            }
-            if (curMsg == .twsIncomingMSG$TICK_GENERIC) {
-                contents <- readBin(con, character(), 4)
-                eventTickGeneric(curMsg,contents)
-            }
-            if (curMsg == .twsIncomingMSG$TICK_STRING) {
-                contents <- readBin(con, character(), 4)
-                eventTickString(curMsg,contents)
-                if(snapshot == '1') 
-                  waiting <- FALSE
-            }
-            if (curMsg == .twsIncomingMSG$TICK_EFP) {
-                contents <- readBin(con, character(), 13)
-                cat('<efp> ')
-                cat(curMsg,paste(contents),'\n')
-            }
-            flush.console()
+          if (curMsg == .twsIncomingMSG$ERR_MSG) {
+              if (!errorHandler(con, verbose, OK = c(165, 300, 366, 2104,2106,2107))) {
+                cat("\n")
+                stop("Unable to complete market data request")
+              }
+          }
+          if (curMsg == .twsIncomingMSG$TICK_PRICE) {
+              contents <- readBin(con, character(), 6)
+              eventTickPrice(curMsg,contents,timeStamp,file)
+          }
+          if (curMsg == .twsIncomingMSG$TICK_SIZE) {
+              contents <- readBin(con, character(), 4)
+              eventTickSize(curMsg,contents,timeStamp,file)
+          }
+          if (curMsg == .twsIncomingMSG$TICK_OPTION) {
+              contents <- readBin(con, character(), 5)
+              eventTickOption(curMsg,contents,timeStamp,file)
+          }
+          if (curMsg == .twsIncomingMSG$TICK_GENERIC) {
+              contents <- readBin(con, character(), 4)
+              eventTickGeneric(curMsg,contents,timeStamp,file)
+          }
+          if (curMsg == .twsIncomingMSG$TICK_STRING) {
+              contents <- readBin(con, character(), 4)
+              eventTickString(curMsg,contents,timeStamp,file)
+              if(snapshot == '1') 
+                waiting <- FALSE
+          }
+          if (curMsg == .twsIncomingMSG$TICK_EFP) {
+              contents <- readBin(con, character(), 13)
+              cat('<efp> ')
+              cat(curMsg,paste(contents),'\n')
+          }
+          flush.console()
         }
-    }
-#    if (missing(callback)) {
-#        cm <- matrix(response, nc = 9, byrow = TRUE)
-#        cm[, 8] <- ifelse(cm[, 8] == "false", 0, 1)
-#        dts <- gsub("(\\d{4})(\\d{2})(\\d{2})", "\\1-\\2-\\3", 
-#            cm[, 1], perl = TRUE)
-#        if (!missing(file)) {
-#            cm[, 1] <- dts
-#            write.table(cm, file = file, quote = FALSE, row.names = FALSE, 
-#                col.names = FALSE, sep = ",")
-#            invisible(return())
-#        }
-#        x <- xts(matrix(as.numeric(cm[, -1]), nc = 8), order.by = as.POSIXct(dts))
-#        colnames(x) <- c("Open", "High", "Low", "Close", "Volume", 
-#            "WAP", "hasGaps", "Count")
-#        xtsAttributes(x) <- list(from = req.from, to = req.to, 
-#            src = "IB", updated = Sys.time())
-#        return(x)
-#    }
-#    else if (is.null(callback)) {
-#        return(c(header, response))
-#    }
-#    else {
-#        FUN <- match.fun(callback)
-#        return(FUN(c(header, response)))
-#    }
+      }
+    } else CALLBACK(con,...)
 }
 
+
+`writeMktData` <- function(con,file='',timeStamp,...)
+{
+
+}
