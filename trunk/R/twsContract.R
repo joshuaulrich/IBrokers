4d1c09a665bb4724f69c5dc1cf33f342f9c10580
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2008-05-13 22:15:52 +0000

    new twsContractDetails work

--- trunk/R/twsContract.R
+++ trunk/R/twsContract.R
@@ -21,3 +21,29 @@ function(symbol,sectype,exch,primary,expiry,strike,
            )
 }
 
+
+`twsContractDetails` <-
+function(symbol,sectype,expiry,right,exch,
+         currency,local,cusip,maturity,issue_date,
+         ratings,bond_type,coupon_type,desc_append,
+         next_option_date,next_option_type,notes,
+         market_name, trading_class, multiplier,
+         order_types, valid_exchanges)
+{
+  structure(
+            list(symbol=symbol,
+                 sectype=sectype,
+                 exch=exch,
+                 primary=primary,
+                 expiry=expiry,
+                 strike=strike,
+                 currency=currency,
+                 right=right,
+                 local=local,
+                 multiplier=multiplier,
+                 combo_legs_desc=combo_legs_desc,
+                 comboleg=comboleg,
+                 include_expired=include_expired),
+            class='twsContract'
+           )
+}
