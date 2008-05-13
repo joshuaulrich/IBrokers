4c9bcdecad4bd41786eb5a9cc736a7c2eba68c27
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-05-13 21:10:30 +0000

    changed default from IDEAL to IDEALPRO

--- R/twsCurrency.R
+++ R/twsCurrency.R
@@ -1,5 +1,5 @@
 `twsCurrency` <-
-function (symbol,currency="USD",exch="IDEAL", primary="", strike = "0.0", 
+function (symbol,currency="USD",exch="IDEALPRO", primary="", strike = "0.0", 
     right = "", local = "", multiplier = "", include_expired = "0") 
 {
     twsContract(symbol, "CASH", exch, primary, expiry = "", strike, 
