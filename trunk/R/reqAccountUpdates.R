78dc88525aeaaee615761e75df0b8e5f0b93eb8c
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2008-07-16 15:08:15 +0000

    reqAccountUpdates now updates the portfolio value -- courtesy of Adrian Dragulescu

--- trunk/R/reqAccountUpdates.R
+++ trunk/R/reqAccountUpdates.R
@@ -10,7 +10,7 @@ function (conn, account="1")
   writeBin(as.character(1), con)
   writeBin(as.character(account), con)
   waiting <- TRUE
-  msg <- NULL
+  msg <- msgPV <- NULL
   while (waiting) {
     curMsg <- readBin(con, character(), 1)
     if (curMsg == .twsIncomingMSG$ERR_MSG) {
@@ -23,14 +23,28 @@ function (conn, account="1")
     if (curMsg == .twsIncomingMSG$ACCT_VALUE) {
       msg <- c(msg,readBin(con, character(), 4)[-1])
     }
+    if (curMsg == .twsIncomingMSG$PORTFOLIO_VALUE) {
+      # code contributed courtesy of Adrian Dragulescu.  Thanks
+      msgPV <- readBin(con, character(), 18)[-c(1,18)]
+    }
     if (curMsg == .twsIncomingMSG$ACCT_UPDATE_TIME) {
-      waiting <- FALSE
-      writeBin(.twsOutgoingMSG$REQ_ACCOUNT_DATA, con)
-      writeBin(VERSION, con)
-      writeBin(as.character(as.integer(0)), con)
-      writeBin(as.character(account), con)
+      if(length(msgPV > 1)) {
+        waiting <- FALSE
+        writeBin(.twsOutgoingMSG$REQ_ACCOUNT_DATA, con)
+        writeBin(VERSION, con)
+        writeBin(as.character(as.integer(0)), con)
+        writeBin(as.character(account), con)
+      }
     }
   }
-  data.frame(matrix(msg,nc=3,byrow=TRUE))
+  
+  msgAV <- data.frame(matrix(msg,nc=3,byrow=TRUE))
+  colnames(msgAV) <- c("key","value","currency")
+
+  msgPV <- data.frame(matrix(msgPV, nc=length(msgPV), byrow=TRUE)) 
+  colnames(msgPV) <- c("ContractId","Symbol","secType",
+                       "Expiry","Strike","Right","Multiplier","primaryExch","localSymbol",
+                       "Currency","Position","marketPrice","marketValue","averageCost","unrealizedPNL","realizedPNL")
+  list(accountValue=msgAV, portfolioValue=msgPV)
 }
 
