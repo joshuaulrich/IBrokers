402a28c06a62fd0edf2d45411ec8fdbde7547727
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-07-21 16:04:30 +0000

    fixed twsOption behavior using wrong expiry argument if missing - now required. updated twsOption docs to be more useful.  Included comments about TWS API requirements

--- R/twsOption.R
+++ R/twsOption.R
@@ -3,7 +3,6 @@ function(local,expiry,
          strike,right="C",exch="SMART",primary="",
          currency="USD",symbol="",include_expired="0")
 {
-  if(missing(expiry)) expiry <- format(Sys.Date(),'%Y%m')
   twsContract(symbol,"OPT",exch,primary,
               expiry,strike,currency,right,
               local,multiplier="100",NULL,NULL,
