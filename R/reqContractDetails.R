cd18bd1ae46fbf8278ed4dc469c77724c7f8fde5
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2009-08-04 17:29:06 +0000

    o   eWrapper.data now takes an arg for the number of contracts     to build internal data list.
    o   eWrapper.data now uses an xts object internally
    
    o   TWS error messages are printed to the screen, and return a value
        when called for internal handling if applicable
    
    o   reqContractDetails now smarter to return a list
    
    o   data is now initialized at the point of eWrapper creation (if applicable)
        This used to occur internal to the CALLBACK or req* calls

--- R/reqContractDetails.R
+++ R/reqContractDetails.R
@@ -71,54 +71,16 @@ function(conn, Contract, reqId="1", conId="", verbose=FALSE,
     while (TRUE) {
       socketSelect(list(con), FALSE, NULL) 
       curMsg <- readBin(con, character(), 1)
-     
-      contracts[[length(contracts)+1]] <- processMsg(curMsg, con, eW, timestamp, file)
+      if(curMsg != .twsIncomingMSG$CONTRACT_DATA) {
+        processMsg(curMsg, con, eW, timestamp, file)
+        if(curMsg == .twsIncomingMSG$CONTRACT_DATA_END)
+          break      
+      }
+      if(curMsg == .twsIncomingMSG$CONTRACT_DATA) {
+      contracts[[length(contracts)+1]] <-
+        processMsg(curMsg, con, eW, timestamp, file)
+      }
 
-      if(curMsg == .twsIncomingMSG$CONTRACT_DATA_END)
-        break      
     }
-#        if (curMsg == .twsIncomingMSG$ERR_MSG) {
-#          if (!errorHandler(con, verbose, OK = c(165, 300, 366, 2104, 2106, 2107))) {
-#            cat("\n")
-#            stop("Unable to complete ContractDetails request")
-#           }
-#        } else 
-#        if (curMsg == .twsIncomingMSG$CONTRACT_DATA) {
-#          msg <- c(msg, list(readBin(con, character(), 20+offsetReqId)))
-#          if(as.numeric(VERSION) <= 4) waiting <- FALSE
-#        } else
-#        if (as.numeric(VERSION) > 4 && curMsg == .twsIncomingMSG$CONTRACT_DATA_END) {
-#          readBin(con, character(), 2)
-#          break
-#        } else {
-#          # catch all the rest.
-#          processMsg(curMsg, con, eWrapper, timestamp, file, ...)
-#        }
-#    }
     return(contracts)
-#    lapply(msg, function(msg) {
-#    twsContractDetails(version=msg[1],
-#                       # reqId = msg[2],
-#                       contract=twsContract(symbol=msg[2+offsetReqId],
-#                                            sectype=msg[3+offsetReqId],
-#                                            expiry=msg[4+offsetReqId],
-#                                            primary=msg[20+offsetReqId],
-#                                            strike=msg[5+offsetReqId],
-#                                            right=msg[6+offsetReqId],
-#                                            exch=msg[7+offsetReqId],
-#                                            currency=msg[8+offsetReqId],
-#                                            multiplier=msg[14+offsetReqId],
-#                                            include_expired="",
-#                                            combo_legs_desc="", comboleg="",
-#                                            local=msg[9+offsetReqId]),
-#                       marketName=msg[10+offsetReqId],
-#                       tradingClass=msg[11+offsetReqId],
-#                       conId=msg[12+offsetReqId],
-#                       minTick=msg[13+offsetReqId],
-#                       orderTypes=unlist(strsplit(msg[15+offsetReqId],",")),
-#                       validExchanges=unlist(strsplit(msg[16+offsetReqId],",")),
-#                       priceMagnifier=msg[17+offsetReqId],
-#                       underConId=msg[18+offsetReqId],
-#                       longName=msg[19+offsetReqId])
-#     })
 }
