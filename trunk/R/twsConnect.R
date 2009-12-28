e272484ea054417314d1ec87c8d82304b11b40a3
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2009-12-28 15:18:57 +0000

    o twsConnect2 client/server test mismatch fixed

--- trunk/R/twsConnect.R
+++ trunk/R/twsConnect.R
@@ -26,15 +26,13 @@ twsConnect2 <- function(clientId=1, host="localhost",
      while(TRUE) {
        curMsg <- readBin(s, character(), 1)
        if(length(curMsg) > 0) {
-         if(curMsg == CLIENT_VERSION) {
-           CONNECTION_TIME <- readBin(s,character(),1)
-         } 
+         if(curMsg == .twsIncomingMSG$ERR_MSG) {
+           if(!errorHandler(s,verbose)) stop() 
+         }
+         CONNECTION_TIME <- readBin(s,character(),1)
          if(curMsg == .twsIncomingMSG$NEXT_VALID_ID) {
            NEXT_VALID_ID <- readBin(s,character(),2)[2]
            break
-         }
-         if(curMsg == .twsIncomingMSG$ERR_MSG) {
-           if(!errorHandler(s,verbose)) stop() 
          }
        }
        if(Sys.time()-start.time > timeout) {
