71db9b4229c22ced9f4332eee99d8cd404cc095d
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2010-12-16 18:06:54 +0000

    o  moved .cancelOrder to cancelOrder. This now just sends    the cancel request and returns.  ..cancelOrder is now the old    method if anyone cares. o  placeOrder now returns the orderId as an integer (instead of NULL)    also assigns to the GlobalEnv the last orderId in the variable    .Last.orderId o  new twsCFD for Contract For Difference contracts. These aren't available    to U.S. based traders, but for IBroker's international users it may    come in handy:
       e.g. reqContractDetails(ibg, twsCFD("CBA","SNFE",currency="AUD"))
    o  upped version to 0.3-2

--- R/twsEquity.R
+++ R/twsEquity.R
@@ -1,4 +1,4 @@
-`twsEquity` <-
+twsSTK <- twsEquity <-
 function(symbol,exch="SMART",primary="",strike='0.0',
          currency='USD',right='',local='',multiplier='',include_expired='0',conId=0)
 {
@@ -6,5 +6,10 @@ function(symbol,exch="SMART",primary="",strike='0.0',
               currency,right,local,multiplier,NULL,NULL,include_expired)
 }
 
+twsCFD <- function(symbol, exch, primary="", strike="",
+                   currency, right="", local="", multiplier="",
+                   include_expired="0", conId=0) {
+  twsContract(conId,symbol,'CFD',exch,primary,expiry='',strike,
+              currency,right,local,multiplier,NULL,NULL,include_expired)
+}
 
-`twsSTK` <- twsEquity
