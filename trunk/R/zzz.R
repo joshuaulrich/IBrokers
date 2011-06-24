5c09f867fe754f8bd969625a10d25074f8e32616
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2011-06-24 19:21:44 +0000

    o changed some cat() to message() in the .onLoad call   Still verbose, and will always issue a warning

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -1,6 +1,6 @@
 `.onLoad` <- function(lib,pkg) {
-  cat("IBrokers version 0.9-1:\n")
-  cat("Implementing API Version 9.64\n\n")
+  message("IBrokers version 0.9-2:\n")
+  message("Implementing API Version 9.64\n\n")
   cat("This software comes with NO WARRANTY.  Not intended for production use!\n")
-  cat("See ?IBrokers for details\n")
+  message("See ?IBrokers for details\n")
 }
