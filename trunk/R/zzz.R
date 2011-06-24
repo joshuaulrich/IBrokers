b0f4d944b1216144cbc132b6f92f9b98bf0e8262
jeff.a.ryan <jeff.a.ryan@07fd740e-ea46-0410-9c1d-63be55e7407d>
2011-06-24 19:23:34 +0000

    o  removed \n from message calls

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -1,6 +1,6 @@
 `.onLoad` <- function(lib,pkg) {
-  message("IBrokers version 0.9-2:\n")
-  message("Implementing API Version 9.64\n\n")
+  message("IBrokers version 0.9-2:")
+  message("Implementing API Version 9.64")
   cat("This software comes with NO WARRANTY.  Not intended for production use!\n")
-  message("See ?IBrokers for details\n")
+  message("See ?IBrokers for details")
 }
