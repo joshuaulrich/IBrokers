b45553842f1bfc49e47dd4f2ca9f3ee6b192c60d
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2011-08-18 22:14:50 +0000

    o  error messages now are sent to stderr() by default. This results in    something more like an error in the terminal, but prevents writes    to csv when dumping to a file. o  new e_update_mkt_depth_csv and related eWrapper.MktDepth.CSV function    for writing order book data to a file.  File format isn't yet stable,    but is in testing o  bumped version to 0.9-4 (following 0.9-3 on CRAN)

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
-`.onLoad` <- function(lib,pkg) {
-  message("IBrokers version 0.9-2:")
+`.onLoad` <- function(libname,pkgname) {
+  message("IBrokers version 0.9-4:")
   message("Implementing API Version 9.64")
   cat("This software comes with NO WARRANTY.  Not intended for production use!\n")
   message("See ?IBrokers for details")
