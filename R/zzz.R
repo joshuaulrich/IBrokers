4ff2ddca8a2ea7802c18cf2ef0b913981e9ca722
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2012-09-28 18:28:43 +0000

    o  removed some remaining .Internal references in the comments o  increased version to 0.9-10 o  updated startup message for new version number o  patched NEXT_VALID_ID capture on connection. Thanks to Chip Kent    for the report and patch.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 `.onLoad` <- function(libname,pkgname) {
-  packageStartupMessage("IBrokers version 0.9-7.  Implementing API Version 9.64")
+  packageStartupMessage("IBrokers version 0.9-10.  Implementing API Version 9.64")
   do.call("cat",list("\nIBrokers comes with NO WARRANTY.  Not intended for production use!\n\n"))
   packageStartupMessage("See ?IBrokers for details.")
 }
 
-.internal <- eval(parse(text=".Internal"))
