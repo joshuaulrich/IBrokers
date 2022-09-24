9df50d293635a9d4f9a9a0a5063b3bb695a7efa4
Wessel dR <4012809+Wesseldr@users.noreply.github.com>
2022-09-24 21:36:19 +0200

    Implement reqManagedAccts() for individual & family
    See #35.
    
    Co-authored-by: J.W. de Roode

--- R/reqManagedAccts.R
+++ R/reqManagedAccts.R
@@ -1,12 +1,37 @@
+.reqManagedAccts <-
+    function(conn) {
+        if(!isConnected(conn))
+            stop('peer has gone away. check your IB connection',call.=FALSE)
+        if(!is.twsConnection(conn))
+            stop('requires twsConnection object')
+
+        con <- conn[[1]]
+
+        VERSION <- "1"
+        outgoing <- c(.twsOutgoingMSG$REQ_MANAGED_ACCTS, VERSION)
+
+        writeBin(outgoing, con)
+}
+
 reqManagedAccts <- function(twsconn) {
-  if( !is.twsConnection(twsconn))
-    stop('invalid twsConnection')
+  .reqManagedAccts(twsconn)
+  con <- twsconn[[1]]
+  e_managed_acc <- eWrapper()
+  while (isConnected(twsconn)) {
+      socketSelect(list(con), FALSE, NULL)
+      curMsg <- readBin(con, character(), 1)
+      accounts_raw <- processMsg(curMsg,
+                                con,
+                                eWrapper=e_managed_acc,
+                                twsconn=twsconn,
+                                timestamp=NULL, file="")
+      if(curMsg == .twsIncomingMSG$MANAGED_ACCTS)
+          break
+  }
+  # Clean up ending "," when multiple accounts are reported.
+  accounts <- if(endsWith(accounts_raw[3], ",")) substring(accounts_raw[3],1, nchar(accounts_raw[3]) - 1) else accounts_raw[3]
 
-  VERSION <- "1"
-
-  writeBin(c(.twsOutgoingMSG$REQ_MANAGED_ACCTS,
-             VERSION), 
-           twsconn[[1]])
+  return(accounts)
 }
 
 requestFA <- function(twsconn, faDataType) {
@@ -16,8 +41,8 @@ requestFA <- function(twsconn, faDataType) {
   VERSION <- "1"
 
   writeBin(c(.twsOutgoingMSG$REQ_FA,
-             VERSION, 
-             as.character(faDataType)), 
+             VERSION,
+             as.character(faDataType)),
            twsconn[[1]])
 }
 
@@ -31,5 +56,5 @@ replaceFA <- function(twsconn, faDataType, xml) {
               VERSION,
               as.character(faDataType),
               as.character(xml)),
-           twsconn[[1]]) 
+           twsconn[[1]])
 }
